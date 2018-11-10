ca00e666f0dccf40cb5037c2f909d0c2a7cd986f
Victor Chang <v.chang.yu@gmail.com>
2018-11-10 09:19:34 -0300

    Initial Commit

---
+++ SliderMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderMenu
+//
+//  Created by Victor Chang on 10/11/2018.
+//  Copyright © 2018 Victor Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
